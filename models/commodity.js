82ccab9b5a18f880df26ec2e4ee4eccc7e92570c
Ilya Bohaslauchyk <bohaslauchyk@gmail.com>
2016-07-06 20:57:00 +0300

    Started the database

---
+++ models/commodity.js
@@ -0,0 +1,9 @@
+var mongoose = require("libs/mongoose");
+
+var Schema = mongoose.Schema;
+
+var commoditySchema = new Schema ({
+
+});
+
+module.exports = mongoose.model ("commodity", commoditySchema);
