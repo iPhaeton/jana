6520f8d5303f28c401bcc0e06b77bfbb2ff78d97
Ilya Bohaslauchyk <3751950@gmail.com>
2016-07-07 17:28:21 +0300

    Made a schema for a commodity

--- models/commodity.js
+++ models/commodity.js
@@ -3,7 +3,24 @@ var mongoose = require("libs/mongoose");
 var Schema = mongoose.Schema;
 
 var commoditySchema = new Schema ({
-
+  category: {
+    type: String
+  },
+  name: {
+    type: String
+  },
+  price: {
+    type: String
+  },
+  amount: {
+    type: String
+  },
+  description: {
+    type: String
+  },
+  specs: {
+    type: Object
+  }
 });
 
 module.exports = mongoose.model ("commodity", commoditySchema);
