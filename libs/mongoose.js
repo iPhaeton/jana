768367d702be60b58e7ccc85c03e148dd4839c21
Ilya Bohaslauchyk <bohaslauchyk@gmail.com>
2016-07-19 19:01:23 +0300

    Changed db addres
    1. Connecting to one of two addresses depending on whether
    process.env.PORT exists

--- libs/mongoose.js
+++ libs/mongoose.js
@@ -1,6 +1,10 @@
 var mongoose = require("mongoose");
 var config = require("config");
 
-mongoose.connect (config.get("mongoose:uri"), config.get("mongoose:options"));
+if (process.env.PORT) {
+    mongoose.connect (config.get("mongoose:uri"), config.get("mongoose:options"));
+} else {
+    mongoose.connect ("mongodb://localhost/jana-shop", config.get("mongoose:options"));
+}
 
 module.exports = mongoose;
