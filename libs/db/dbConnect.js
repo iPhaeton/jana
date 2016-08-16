8683757f895bda0e2cb03875fad235561388fbc6
Ilya Bohaslauchyk <bohaslauchyk@gmail.com>
2016-08-16 21:31:18 +0300

    Change connection to the database
    1. Fixed an issue with multiple error events when the database is
    unavailable

--- libs/db/dbConnect.js
+++ libs/db/dbConnect.js
@@ -13,11 +13,11 @@ module.exports = function (req, res, next) {
             async.whilst(() => {
                 return countAttemptsToConnect < 10;
             }, (callback) => {
-                setTimeout((() => {
-                    return function () {
-                        open(callback);
-                    };
-                })(), 1000 * countAttemptsToConnect);
+                setTimeout(() => {
+                    logger.log("Tyring to connect to the database - " + countAttemptsToConnect);
+                    open(callback);
+                    countAttemptsToConnect++;
+                }, 1000 * countAttemptsToConnect);
             }, (err) => {
                 countAttemptsToConnect = undefined;
                 if (err) callback(err);
@@ -33,7 +33,7 @@ module.exports = function (req, res, next) {
 };
 
 function open (callback) {
-    if (countAttemptsToConnect !== undefined) countAttemptsToConnect++;
+    //var initiatedBy = countAttemptsToConnect;
 
     if (process.env.PORT) {
         mongoose.connect (config.get("mongoose:uri"), config.get("mongoose:options"));
@@ -41,18 +41,28 @@ function open (callback) {
         mongoose.connect ("mongodb://localhost/jana-shop", config.get("mongoose:options"));
     };
 
-    mongoose.connection.on("open", (err) => {
+    mongoose.connection.once ("open", onConnectionOpen);
+    mongoose.connection.once ("error", onConnectionError);
+
+    function onConnectionOpen (err) {
+        mongoose.connection.removeListener("error", onConnectionError);
+
         if(!countAttemptsToConnect) return;
         countAttemptsToConnect = 11;
         callback();
-    });
-    mongoose.connection.on("error", (err) => {
-        if(!countAttemptsToConnect) return;
+    };
+
+    function onConnectionError (err) {
+        //logger.log("initiated by - " + initiatedBy);
+
+        mongoose.connection.removeListener("open", onConnectionOpen);
+
+        if (!countAttemptsToConnect) return;
         if (countAttemptsToConnect > 9) {
             callback(err);
         }
         else callback();
-    });
+    };
 };
 
 function requireModels (callback) {
