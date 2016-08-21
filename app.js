5c94bd57ed434426f51cde60e13d7eb99511f8b3
Ilya Bohaslauchyk <bohaslauchyk@gmail.com>
2016-08-21 11:18:41 +0300

    Change logging of errors in production mode

--- app.js
+++ app.js
@@ -128,7 +128,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
-    error: {}
+    statusCode: err.status || 500
   });
 });
 
