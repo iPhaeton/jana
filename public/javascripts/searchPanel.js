db89df059ee8b02658932b782a1a52ab3e63cde2
Ilya Bohaslauchyk <bohaslauchyk@gmail.com>
2016-08-25 20:48:02 +0300

    Getting values for popup from database

--- public/javascripts/searchPanel.js
+++ public/javascripts/searchPanel.js
@@ -35,21 +35,21 @@ SearchPanelPopupControl = function (name, parent) {
 };
 
 SearchPanelPopupControl.prototype.create = function () {
+    var self = this;
+
     this.button = $(
-        '<div class="search-panel__control-popup" data-toggle="zero-display" data-for="#manufacturers">\
+        '<div class="search-panel__control-popup">\
             <p class="search-panel__control-popup__text">' + this.name + '</p>\
             <span class="glyphicon glyphicon-chevron-down search-panel__control-popup__icon" aria-hidden="true"></span>\
         </div>');
 
-    this.body = $(
-        '<div id="manufacturers" class="search-panel__popup-panel zero-display">\
-            <input type="checkbox"> Checkbox\
-            <input type="checkbox"> Checkbox\
-            <input type="checkbox"> Checkbox\
-            <input type="checkbox"> Checkbox\
-            <input type="checkbox"> Checkbox\
-            <div class="clearfix"></div>\
-        </div>');
+    this.body = $("<div class='search-panel__body-popup zero-display'></div>");
+
+    this.getValues(function (err, values) {
+        for (var value in values) {
+            self.body.append("<input type='checkbox'>" + value);
+        };
+    });
 
     this.elem.append(this.button);
     this.elem.append(this.body);
@@ -57,8 +57,8 @@ SearchPanelPopupControl.prototype.create = function () {
     this.setEvents();
 };
 
-SearchPanelPopupControl.prototype.getValues = function () {
-    
+SearchPanelPopupControl.prototype.getValues = function (callback) {
+    makeDBSearchRequest("/dbsearch?db=Commodity&field=specs." + this.name, callback);
 };
 
 SearchPanelPopupControl.prototype.setEvents = function () {
