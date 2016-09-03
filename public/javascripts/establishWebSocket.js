bcc240cbb30523d2e755314ce8a7918ec23b389b
Ilya Bohaslauchyk <bohaslauchyk@gmail.com>
2016-09-03 14:28:56 +0300

    SocketJS
    Have an issue with valitading a session on establishing a socketjs
    connection

--- public/javascripts/establishWebSocket.js
+++ public/javascripts/establishWebSocket.js
@@ -1,4 +1,7 @@
 function establishWebSocket() {
-    var socket = new WebSocket("ws://" + window.location.hostname + ":3000");
-    return socket;
+    /*var socket = new WebSocket("ws://" + window.location.hostname + ":3000");
+    return socket;*/
+
+    var sock = new SockJS("http://" + window.location.hostname + ":3000/sock");
+    //sock.send('test');
 };
--- socket/index.js
+++ socket/index.js
@@ -1,16 +1,19 @@
 var WebSocketServer = new require("ws");
 var socketAuth = require("./socketAuth");
 var logger = new require('libs/logger')(module);
+var sockjs = require("sockjs");
 
 var sockets = {};
 
 module.exports = function (server) {
-    var webSocketServer = new WebSocketServer.Server({
-        server:server
-    });
+    var sock = sockjs.createServer({ sockjs_url: 'http://cdn.jsdelivr.net/sockjs/1.0.1/sockjs.min.js' });
 
-    webSocketServer.on("connection", function (socket) {
-        socketAuth(socket.upgradeReq, function (err, results) {
+    sock.installHandlers(server, {prefix:'/sock'});
+
+    sock.on("connection", function (socket) {
+        /*socketAuth(socket.upgradeReq, function (err, results) {
+            logger.log ("SocjetJS connection");
+
             var id = Math.random() * Math.random() * Math.random();
             sockets[id] = results || {session: null, user: null};
             sockets[id].socket = socket;
@@ -19,6 +22,6 @@ module.exports = function (server) {
                 logger.log(id + " disconnected");
                 delete sockets[id];
             });
-        });
+        });*/
     });
 };
