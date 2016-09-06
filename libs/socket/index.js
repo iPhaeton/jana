ab9e8a24578d4de6cab0ee29f2e13f4aa560bddf
Ilya Bohaslauchyk <bohaslauchyk@gmail.com>
2016-09-06 20:35:20 +0300

    A sock router is an object

--- libs/socket/index.js
+++ libs/socket/index.js
@@ -2,9 +2,11 @@ var socketAuth = require("./socketAuth");
 var logger = new require('libs/logger')(module);
 var sockjs = require("sockjs");
 var changeSocket = require("libs/changeSocket");
-var sockRouter = require("libs/socket/sockRouter");
+var Router = require("libs/socket/sockRouter");
 
 module.exports = function (server) {
+    var router = new Router();
+
     var sock = sockjs.createServer({ sockjs_url: "/vendor/bower_components/sockjs/sockjs.min.js"});
 
     sock.installHandlers(server, {prefix:'/sock'});
@@ -14,7 +16,9 @@ module.exports = function (server) {
 
         changeSocket(socket.url, "socket", socket);
 
-        socket.on("data", sockRouter);
+        socket.on("data", (reqData) => {
+            router.route(socket, reqData);
+        });
 
         socket.on("close", function () {
             logger.log(socket.url + " disconnected");
--- libs/socket/sockRouter.js
+++ libs/socket/sockRouter.js
@@ -1,20 +1,34 @@
+"use strict";
+
 var url = require("url");
 var find = require("libs/find");
 var gatherDocsById = require("libs/gatherDocsById");
 
-module.exports = function (reqData) {
-    var reqData = JSON.parse(reqData);
+class Router {
+    constructor () {};
 
-    var query = url.parse(reqData.request, true).query;
+    route (socket, reqData) {
+        this.socket = socket;
 
-    var result = find(query);
+        var reqData = JSON.parse(reqData);
 
-    if (result.size) {
-        gatherDocsById(result, (err, commodities) => {
-            if (err) this.write(JSON.stringify({type: "searchResult", data: "Error"}));
-            else this.write(JSON.stringify({type: "searchResult", data: commodities}));
-        });
-    } else {
-        this.write(JSON.stringify({type: "searchResult", data: null}));
-    }
+        var request = url.parse(reqData.request, true);
+
+        this[request.pathname](request.query);
+    };
+    
+    ["/search"] (query) {
+        var result = find(query);
+
+        if (result.size) {
+            gatherDocsById(result, (err, commodities) => {
+                if (err) this.socket.write(JSON.stringify({type: "searchResult", data: "Error"}));
+                else this.socket.write(JSON.stringify({type: "searchResult", data: commodities}));
+            });
+        } else {
+            this.socket.write(JSON.stringify({type: "searchResult", data: null}));
+        };
+    };
 };
+
+module.exports = Router;
