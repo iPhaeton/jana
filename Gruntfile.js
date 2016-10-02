c2d4875841ea3e3023368b8af148f12039d23810
Ilya Bohaslauchyk <bohaslauchyk@gmail.com>
2016-10-02 23:57:42 +0300

    Uglifying task
    Runs properly

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,42 @@
-module.exports = function () {
+module.exports = function (grunt) {
     //Configuration
     grunt.initConfig({
         uglify: {
             my_target: {
-                files: {
+                files: [{
                     expand: true,
-                    cwd: 'public/javascript',
-                    src: '**/*.js',
-                    dest: 'public/javascript'
-                }
+                    cwd: 'public/javascripts/babel',
+                    src: ['**/*.js'],
+                    dest: 'public/javascripts',
+                    ext: '.min.js'
+                }]
             }
+        },
+
+        babel: {
+            dist: {
+                options: {
+                    presets: ['es2015']
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'public/javascripts',
+                    src: ['**/*.js'],
+                    dest: 'public/javascripts/babel',
+                }]
+            }
+        },
+
+        clean: {
+            folder: ['public/javascripts/babel']
         }
     });
 
     //Plugins
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-babel');
+    grunt.loadNpmTasks('grunt-contrib-clean');
 
     //Running tasks
+    grunt.registerTask('uglify-js', ['babel', 'uglify', 'clean']);
 };
